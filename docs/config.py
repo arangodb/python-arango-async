a65ce1713a8e4b2d0149f9521e17a6248ce29a83
Alex Petenchea <alex.petenchea@gmail.com>
2024-03-14 12:59:15 +0200

    Adding requirements for docs

--- docs/config.py
+++ docs/config.py
@@ -1,4 +1,11 @@
 project = "python-arango-async"
 copyright_notice = "ArangoDB"
 author = "Alexandru Petenchea, Anthony Mahanna"
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+master_doc = "index"
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+python-arango-async
+-------------------
+
+Welcome to the documentation for **python-arango-async**, a Python driver for ArangoDB_.
+
+**The driver is currently work in progress and not yet ready for use.**
+
+.. _ArangoDB: https://www.arangodb.com
