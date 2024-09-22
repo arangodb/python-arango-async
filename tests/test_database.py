8d07827f19264acd8a29121100d97f50d65433aa
Alex Petenchea <alex.petenchea@gmail.com>
2024-09-22 19:53:23 +0300

    Introducing Database class

---
+++ tests/test_database.py
@@ -0,0 +1,15 @@
+import pytest
+
+from arangoasync.auth import Auth
+from arangoasync.client import ArangoClient
+
+
+@pytest.mark.asyncio
+async def test_client_basic_auth(url, sys_db_name, root, password):
+    auth = Auth(username=root, password=password)
+
+    # TODO create a test database and user
+    async with ArangoClient(hosts=url) as client:
+        db = await client.db(sys_db_name, auth_method="basic", auth=auth, verify=True)
+        status = await db.status()
+        assert status["server"] == "arango"
